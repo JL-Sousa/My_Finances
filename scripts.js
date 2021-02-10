9fd707952794b9daea251604fee6b47ca8a8c2b4
jonyelson <jonyelson20@gmail.com>
2021-02-10 15:08:05 -0400

    atualização no scripts.js

--- scripts.js
+++ scripts.js
@@ -37,6 +37,12 @@ const transactions = [
     description: 'Internet',
     amount: -20000,
     date: '23/01/2021'
+  },
+  {
+    id: 4,
+    description: 'App',
+    amount: 200000,
+    date: '23/01/2021'
   }
 ]
 
@@ -55,17 +61,39 @@ const Transaction = {
 // Substituir os dados do HTML com os dados JS
 
 const DOM = {
-  innerHTMLTransaction() {
+  transactionsContainer: document.querySelector('#data-table tbody'),
+
+  addTransaction(transaction, index) {
+    console.log(transaction)
+    const tr = document.createElement('tr')
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+    DOM.transactionsContainer.appendChild(tr)
+  },
+
+  innerHTMLTransaction(transaction) {
+    const CSSclass = transaction.amount > 0 ? 'income' : 'expense'
+
+    const amount = Utils.formatCurrency(transaction.amount)
     
     const html = `
-    <tr>
-      <td class="date">Luz</td>
-      <td class="expense">R$ 500</td>
-      <td class="date">23/01/2021</td>
+      <td class="description">${transaction.description}</td>
+      <td class=${CSSclass}>${transaction.amount}</td>
+      <td class="date">${transaction.date}</td>
       <td>
           <img src="./assets/minus.svg" alt="Remover transação">
       </td>
-    </tr> 
     `
+
+    return html
   }
 }
+
+const Utils = {
+  formatCurrency(value) {
+    const signal = Number(value) < 0 ? '-' : ''
+  }
+}
+
+transactions.forEach(function(transaction) {
+  DOM.addTransaction(transaction)
+})
